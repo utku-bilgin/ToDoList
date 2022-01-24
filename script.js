c6aae5e0591e4bbc38d97db49184d38f9188dbc6
utku-bilgin <h.utkubilgin@gmail.com>
2022-01-24 22:49:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-let textToDo = document.getElementById('textToDo')
+let textToDo = document.getElementById('textToDo');
 let addToDoButton = document.getElementById('addToDo');
-let clearToDoButton = document.getElementById('clearToDo')
-let containerToDo = document.getElementById('containerToDo')
+let clearToDoButton = document.getElementById('clearToDo');
+let containerToDo = document.getElementById('containerToDo');
 
-addToDoButton.addEventListener('click', function(){
+addToDoButton.addEventListener('click', add = () => {
 
     let textList = document.createElement('p');
     containerToDo.appendChild(textList);
-    textList.classList.add('textListStyle')
+    textList.classList.add('textListStyle');
     textList.innerHTML = textToDo.value;
-    textToDo.value = "" ;
+    textToDo.value = "";
 
-    textList.addEventListener('click', function(){
-        textList.style.textDecoration = 'line-through'
-    })
+    textList.addEventListener('click', line = () => {
+        textList.style.textDecoration = 'line-through';
+    });
 
-    textList.addEventListener('dblclick', function(){
+    textList.addEventListener('dblclick', clearChild = () => {
         containerToDo.removeChild(textList);
-    })
+    });
 
-    clearToDoButton.addEventListener('click', function(){
+    clearToDoButton.addEventListener('click', clearAll = () => {
         textList.remove();
-    })
+    });
 
 
-})
+});
